d4617f2845b328bfc34ec82246a20c347652898b
wenderf5 <wenderfabianoalves@gmail.com>
2025-02-19 22:35:30 -0300

    feat: empty cart

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -2,8 +2,8 @@ import style from './title.module.css';
 
 export function Title() {
     return (
-        <section className={style.container_title}>
+        <div className={style.container_title}>
             <h1>Seu carrinho</h1>
-        </section>
+        </div>
     )
 }
